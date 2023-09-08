180463ce6fae5a151bb5d80f4317bf3c46ea013d
Nnoggie <27819512+Nnoggie@users.noreply.github.com>
2023-09-08 14:30:13 +0200

    add some sounds but commented out

--- sounds.lua
+++ sounds.lua
@@ -11,6 +11,32 @@ addon.sounds = {
   5350036, -- attack
   5342387,
   5340605,
+
+  ---------------
+  --- BLIZZARD
+  ---------------
+  -- 567478,  -- Ready Check, also used by wow in the SFX channel by default
+  -- 876098,  -- Blizzard Raid Emote
+
+  ---------------
+  --- DBM
+  ---------------
+  -- 543587,  -- Algalon: Beware!
+  -- 552035,  -- BB Wolf: Run Away
+  -- 552503,  -- Illidan: Not Prepared
+  -- 1412178, -- Illidan: Not Prepared2
+  -- 553193,  -- Kil'Jaeden: Destruction
+  -- 554236,  -- Loatheb: I see you
+  -- 566558,  -- Night Elf Bell
+  -- 569200,  -- PvP Flag
+  -- 546633,  -- C'Thun: You Will Die!
+  -- 551703,  -- Headless Horseman: Laugh
+  -- 553050,  -- Kaz'rogal: Marked
+  -- 553566,  -- Lady Malande: Flee
+  -- 555337,  -- Milhouse: Light You Up
+  -- 563787,  -- Void Reaver: Marked
+  -- 564859,  -- Yogg Saron: Laugh
+
 }
 
 function addon:AddLSMSounds()
